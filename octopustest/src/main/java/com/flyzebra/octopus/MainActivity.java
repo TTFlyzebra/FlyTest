deddc11c30fe326bec6660e6bc8ecd3e822ea5b2
FlyZebra <flycnzebra@gamil.com>
2021-01-31 11:38:21 +0800

    add permission

--- octopustest/src/main/java/com/flyzebra/octopus/MainActivity.java
+++ octopustest/src/main/java/com/flyzebra/octopus/MainActivity.java
@@ -1,17 +1,45 @@
 package com.flyzebra.octopus;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.flyzebra.octopus.utils.FlyLog;
 
 public class MainActivity extends AppCompatActivity {
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    private static int REQUEST_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            for(int i=0;i<PERMISSIONS_STORAGE.length;i++)
+            if (ActivityCompat.checkSelfPermission(this, PERMISSIONS_STORAGE[i]) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_PERMISSION_CODE);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            FlyLog.d("");
+        }
     }
 
     public void gpstest(View view) {
@@ -24,5 +52,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void camera2test(View view) {
         startActivity(new Intent(this,Camera2Activity.class));
+    }
+
+    public void networktest(View view) {
     }
 }
