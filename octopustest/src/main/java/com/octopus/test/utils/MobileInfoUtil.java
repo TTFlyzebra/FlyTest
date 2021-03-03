e967f563751ef6d0f6b84e6bd529064121a79842
FlyZebra <flycnzebra@gamil.com>
2021-03-03 11:24:20 +0800

    fixed gps test

--- octopustest/src/main/java/com/octopus/test/utils/MobileInfoUtil.java
+++ octopustest/src/main/java/com/octopus/test/utils/MobileInfoUtil.java
@@ -6,12 +6,8 @@ package com.octopus.test.utils; /**
  * Description:
  */
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.telephony.CellLocation;
 import android.telephony.TelephonyManager;
-import android.telephony.cdma.CdmaCellLocation;
-import android.telephony.gsm.GsmCellLocation;
 
 /**
  * 获取手机信息工具类
@@ -61,23 +57,6 @@ public class MobileInfoUtil {
         } catch (Exception e) {
             e.printStackTrace();
             return "";
-        }
-    }
-
-    public static void getCellInfo(Context context) {
-        TelephonyManager tel = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        @SuppressLint("MissingPermission")
-        CellLocation cel = tel.getCellLocation();
-        int nPhoneType = tel.getPhoneType();
-        if (nPhoneType == 2 && cel instanceof GsmCellLocation) {
-            GsmCellLocation gsmCellLocation = (GsmCellLocation) cel;
-            int nGSMCID = gsmCellLocation.getCid();
-            if (nGSMCID > 0) {
-                if (nGSMCID != 65535) {
-                }
-            }
-        }else if (nPhoneType == 2 && cel instanceof CdmaCellLocation) {
-            CdmaCellLocation cdmaCellLocation = (CdmaCellLocation) cel;
         }
     }
 
